e486d449b05884b921105a050c3d48f094ebe6d7
Nils Martel <nils.martel@graphmasters.net>
2019-12-27 09:47:17 +0100

    Get rid of Magic Numbers

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,17 @@
 #[derive(Copy, Clone, Debug)]
 pub struct DateTime(u32);
 
+const OFFSET_YEAR: u32 = 8;
+const MASK_YEAR: u32 = 0o777;
+
 impl DateTime {
     pub fn new(year: u32, month: u32, day: u32, hour: u32, minute: u32) -> {
-        
+
     }
 
     pub fn year(self, year: u32) -> DateTime {
-        self.0 &= !(0o777<< 8);
-        self.0 |= (year & 0o777)<< 8;
+        self.0 &= !(MASK_YEAR<< MASK_YEAR);
+        self.0 |= (year & MASK_YEAR)<< MASK_YEAR;
         self
     }
-}
+t
